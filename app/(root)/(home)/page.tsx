95a9628dbc9cd57f936236126905e2abc67bc8bf
Benjamin Huy Pham <benjaminhpham@gmail.com>
2023-09-23 00:42:01 -0700

    add auth with clerk

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,5 +1,9 @@
-import React from "react";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 }
